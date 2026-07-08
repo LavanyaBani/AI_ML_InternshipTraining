01e3081c40d6b216f9bba6b950a7d100a58536fb
Lavanya Bani <lavanyabani@gmail.com>
2026-07-08 22:36:11 +0530

    path in b_app fixed

--- bp_model/bp_app.py
+++ bp_model/bp_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from pathlib import Path
+import joblib
 BASE_DIR = Path(__file__).resolve().parent
-model = joblib.load(BASE_DIR /"bp_model.pkl")
+
 st.title("BP Prediction")
 Age = st.number_input("Enter your age")
 weight = st.number_input("Enter your weight")
 if st.button("Predict BP"):
-    import joblib
-    model = joblib.load("bp_model.pkl")
+    
+    model = joblib.load(BASE_DIR /"bp_model.pkl")
     prediction = model.predict([[Age, weight]])
     st.write(f"Predicted BP: {prediction[0]}")
