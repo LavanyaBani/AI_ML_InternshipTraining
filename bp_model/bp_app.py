b89cc42c3d775a58c98fe891dfba782c9d7b17b2
Lavanya Bani <lavanyabani@gmail.com>
2026-07-08 22:33:24 +0530

    path in b_app fixed

--- bp_model/bp_app.py
+++ bp_model/bp_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+BASE_DIR = Path(__file__).resolve().parent
+model = joblib.load(BASE_DIR /"bp_model.pkl")
 st.title("BP Prediction")
 Age = st.number_input("Enter your age")
 weight = st.number_input("Enter your weight")
